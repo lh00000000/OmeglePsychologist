5d4a1c4767915e75b79119523ff010f8c3ee888c
Luming Hao <lh00000000@gmail.com>
2015-01-17 02:27:36 -0500

    inc

---
+++ OmegleEliza/src/omegleeliza/OmegleEliza.java
@@ -0,0 +1,17 @@
+package omegleeliza;
+
+import processing.core.PApplet;
+
+
+public class OmegleEliza extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { omegleeliza.OmegleEliza.class.getName() });
+	}
+}
